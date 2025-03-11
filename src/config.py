035e08c369ad77ac0702802ebd22e91b5b2bf191
tinymonster123 <lfhxgs123@gmail.com>
2025-03-12 01:21:55 +0800

    chore: 增添模型名称

--- src/config.py
+++ src/config.py
@@ -11,3 +11,5 @@ class Config:
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
+    BERT_MODEL_NAME = os.getenv("BERT_MODEL_NAME", "paraphrase-multilingual-MiniLM-L12-v2")  # 默认使用多语言模型
+    

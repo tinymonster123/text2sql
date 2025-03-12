a396208dcb912d6e7624c06f9440669a903d60ab
tinymonster123 <lfhxgs123@gmail.com>
2025-03-12 13:30:24 +0800

    chore: 添加 deepseek 环境变量

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SSH_HOST = os.getenv("SSH_HOST")
     SSH_USER = os.getenv("SSH_USER")
@@ -10,8 +11,10 @@ class Config:
     DB_HOST = os.getenv("DB_HOST")
     DB_NAME = os.getenv("DB_NAME")
     DB_USER = os.getenv("DB_USER")
+    DEEPSEEK = os.getenv("DEEPSEEK")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     API_KEY = os.getenv("API_KEY")
     BASE_URL = os.getenv("BASE_URL")
-    BERT_MODEL_NAME = os.getenv("BERT_MODEL_NAME", "paraphrase-multilingual-MiniLM-L12-v2")  # 榛樿浣跨敤澶氳瑷€妯″瀷
-    
+    BERT_MODEL_NAME = os.getenv(
+        "BERT_MODEL_NAME", "paraphrase-multilingual-MiniLM-L12-v2"
+    )
